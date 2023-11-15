9afcc447d02c0f8d1b7ccd27bdaaf378c8bf0abc
Vladimir Serkutyev <vladimirserkutyev@coherentsolutions.com>
2023-11-16 00:36:47 +0300

    first commit

---
+++ app/src/main/java/com/example/andersenhw1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.andersenhw1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.andersenhw1.ui.theme.AndersenHW1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndersenHW1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Vladimir")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndersenHW1Theme {
+        Greeting("Android")
+    }
+}
